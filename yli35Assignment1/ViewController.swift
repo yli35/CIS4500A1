a3d6917268201eaf13c3b5d502941072f4dc9f6c
Yichen Li <yli35@uoguelph.ca>
2018-01-16 23:01:48 -0500

    Initial Commit

---
+++ yli35Assignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yli35Assignment1
+//
+//  Created by 李易宸 on 16/01/2018.
+//  Copyright © 2018 Yichen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
